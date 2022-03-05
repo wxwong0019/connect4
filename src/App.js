9fc61fd571784adfb5eb317b31cc7f14f47d8f41
wing <wongwingchit@gmail.com>
2022-03-04 23:35:13 -0800

    board created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  var initialBoard = [];
+  for(let i=0; i<6; i++){
+    var colArray = [];
+    for(let i=0; i<7; i++){
+      colArray.push(null);
+    }
+    initialBoard.push(colArray);
+  }
+  initialBoard[0][2] = 1
+  const [currentPlayer, setCurrentPlayer] = useState("blue")
+  const [board, setBoard] = useState(initialBoard)
+  
+  const checkBoard = (x,y) => {
+    if(board[x][y]){
+      return true;
+    }
+    return false;
+  }
+  console.log(board)
+  var rowArray = [];
+  for(let i=0; i<6; i++){
+    var colArray= [];
+    for(let j=0; j<7; j++){
+      
+      colArray.push(
+        <div style={ {border:"1px solid black", backgroundColor:'black', width:'10vw', height:'10vw', display:'flex'}}>
+          <div style={{borderRadius:"50%", backgroundColor:'white', display:'flex', width:'70%', height:'70%', marginLeft:'10px', padding:1} }>
+            {checkBoard(i,j) ? <div style={{ backgroundColor:currentPlayer, borderRadius:"50%",flex:1}} /> : null}  
+          </div> 
+
+        </div>
+      )
+    }
+    rowArray.push(
+      <div style={{display:'flex', flexDirection:'row'}}>{colArray}</div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{rowArray}</div>
     </div>
   );
 }
